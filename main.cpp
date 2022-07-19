a6d2efe2c81ed424d63a56d5057fc89d6165b372
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-07-19 13:19:03 +0200

    Add some code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
+#include "nlohmann/json.hpp"
+#include <iostream>
+#include <stdexcept>
+
+
 int main()
 {
+    auto const json =
+    R"(
+    {
+      "key": "value"
+    }
+    )"_json;
+
+    try
+    {
+        auto const val = json["key"].get<std::string>();
+        std::cout << val << '\n';
+    }
+    catch (std::exception const & e)
+    {
+        std::cout << "Error: " << e.what() << '\n';
+    }
+
     return 0;
 }
